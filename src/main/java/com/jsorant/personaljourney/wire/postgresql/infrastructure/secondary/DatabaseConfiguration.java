ddc69865946340529d1a665baff198d752e18a2c
Jérémy Sorant <jsorant@gmail.com>
2024-11-30 20:41:28 +0100

    Fix coverage

--- src/main/java/com/jsorant/personaljourney/wire/postgresql/infrastructure/secondary/DatabaseConfiguration.java
+++ src/main/java/com/jsorant/personaljourney/wire/postgresql/infrastructure/secondary/DatabaseConfiguration.java
@@ -1,5 +1,6 @@
 package com.jsorant.personaljourney.wire.postgresql.infrastructure.secondary;
 
+import com.jsorant.personaljourney.shared.generation.domain.ExcludeFromGeneratedCodeCoverage;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -7,4 +8,5 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = { "com.jsorant.personaljourney" }, enableDefaultTransactions = false)
+@ExcludeFromGeneratedCodeCoverage
 class DatabaseConfiguration {}
